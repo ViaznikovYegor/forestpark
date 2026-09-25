74cd40440b591ea5ab5d1950eaf8de815b241510
Viaznikov Yegor <dexbustakkaunt@gmail.com>
2026-09-25 14:39:13 +0300

    fix alembic env

---
+++ app/core/base.py
@@ -0,0 +1,8 @@
+from app.core.db import Base
+from app.models.infrastructureobject import InfrastructureObject
+
+
+__all__ = [
+    'Base',
+    'InfrastructureObject'
+]
---
+++ app/models/__init__.py
@@ -0,0 +1,15 @@
+from app.models.infrastructureobject import (
+    ObjectType,
+    Material,
+    Hardware,
+    Section,
+    InfrastructureObject
+)
+
+__all__ = [
+    'ObjectType',
+    'Material',
+    'Hardware',
+    'Section',
+    'InfrastructureObject'
+]
